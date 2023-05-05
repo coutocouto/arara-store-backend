d7d3736726dc58296005348f7702879d5f95f6dc
Felipe Couto <couto.felipe2@gmail.com>
2023-05-05 19:29:03 -0300

    style: fazendo exportação de todas entidades

---
+++ src/modules/index.entities.ts
@@ -0,0 +1,9 @@
+import { Address } from '../modules/addresses/entities/address.entity';
+import { Cart } from '../modules/carts/entities/cart.entity';
+import { Favorite } from '../modules/favorites/entities/favorite.entity';
+import { Item } from '../modules/items/entities/item.entity';
+import { Order } from '../modules/orders/entities/order.entity';
+import { Product } from './products/entities/product.entity';
+import { User } from '../modules/users/entities/user.entity';
+
+export { Address, Cart, Favorite, Item, Order, Product, User };
