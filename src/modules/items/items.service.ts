c401cba1b9144692cc5f858d2c6bd00bbf0b163c
Felipe Couto <couto.felipe2@gmail.com>
2023-05-20 21:18:24 -0300

    feature: fazendo os items trazer os produtos com as imagens

--- src/modules/items/items.service.ts
+++ src/modules/items/items.service.ts
@@ -2,13 +2,14 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { Item } from './entities/item.entity';
+import { Image, Product } from '../index.entities';
 
 @Injectable()
 export class ItemsService {
   constructor(
     @Inject('ITEMS_REPOSITORY')
     private itemRepository: typeof Item,
-  ) {}
+  ) { }
 
   async create(createItemDto: CreateItemDto): Promise<Item> {
     return await this.itemRepository.create({ ...createItemDto });
@@ -16,13 +17,31 @@ export class ItemsService {
 
   async findAll(): Promise<Item[]> {
     return await this.itemRepository.findAll<Item>({
-      include: ['product', 'cart'],
+      include: [
+        {
+          model: Product,
+          include: [
+            {
+              model: Image,
+            },
+          ],
+        },
+      ],
     });
   }
 
   async findOne(id: number): Promise<Item> {
     return await this.itemRepository.findByPk<Item>(id, {
-      include: ['product', 'cart'],
+      include: [
+        {
+          model: Product,
+          include: [
+            {
+              model: Image,
+            },
+          ],
+        },
+      ],
     });
   }
 
