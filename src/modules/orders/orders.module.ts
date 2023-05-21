b49541efcad2bc24bd51ae421317e8f557113d71
Felipe Couto <couto.felipe2@gmail.com>
2023-05-20 21:22:04 -0300

    fix: removendo a tabela order items

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -3,12 +3,11 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { DatabaseModule } from '../../db/database.module';
 import { ordersProviders } from './orders.providers';
-import { orderItemsProviders } from './orderItems.providers';
 import { CartsModule } from '../index.module';
 
 @Module({
   imports: [DatabaseModule, forwardRef(() => CartsModule)],
   controllers: [OrdersController],
-  providers: [OrdersService, ...ordersProviders, ...orderItemsProviders],
+  providers: [OrdersService, ...ordersProviders],
 })
-export class OrdersModule {}
+export class OrdersModule { }
