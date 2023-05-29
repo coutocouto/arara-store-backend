22e5362d3f9ef780eb7a286bdf036d31cf63e29e
Felipe Couto <couto.felipe2@gmail.com>
2023-05-29 20:08:06 -0300

    feat: instalando swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NotFoundInterceptor } from './helpers/interceptors/not-found.interceptors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // CORS
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Arara-store API')
+    .setDescription('API da loja de roupas')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   // Pipes
   app.useGlobalPipes(
     new ValidationPipe({
